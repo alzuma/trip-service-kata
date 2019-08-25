568807f4f34b6d330afa2b495cbc536639335e04
Kristaps Vitolins <arch.kristaps.vitolins@gmail.com>
2019-08-25 11:30:03 +0300

    add dep. and fist test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,17 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.List;
+
 public class UserTest {
 
+    @Test
+    public void
+    create_empty_user_friends_should_be_zer() {
+        User user = new User();
+        List<User> friends = user.getFriends();
+        Assertions.assertThat(friends).hasSize(0);
+    }
 }
